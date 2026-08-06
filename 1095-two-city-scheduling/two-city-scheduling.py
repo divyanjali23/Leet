ba5ab8ffabfcb3d7c7ac7d5a4c6849d04d0419bb
Divya <23H41A4549@bvcits.edu.in>
2026-08-06 13:53:10 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (33.49%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.py
+++ 1095-two-city-scheduling/two-city-scheduling.py
@@ -4,7 +4,6 @@ class Solution:
         costs.sort(key=lambda x:x[0]-x[1])
         total=0
         for i in range(n):
-            total+=costs[i][0]
-            total+=costs[i+n][1]
+            total+=costs[i][0]+costs[i+n][1]
         return total 
         
